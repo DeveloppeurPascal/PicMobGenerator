0c769224e31554dcbf991444d378959c6d37be57
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2025-10-16 19:39:03 +0200

    added copyright and timestamp comments

--- src/uDM.pas
+++ src/uDM.pas
@@ -1,3 +1,35 @@
+(* C2PP
+  ***************************************************************************
+
+  Pic Mob Generator
+
+  Copyright 2022-2025 Patrick PREMARTIN under AGPL 3.0 license.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+  THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+  DEALINGS IN THE SOFTWARE.
+
+  ***************************************************************************
+
+  Author(s) :
+  Patrick PREMARTIN
+
+  Site :
+  https://picmobgenerator.olfsoftware.fr/
+
+  Project site :
+  https://github.com/DeveloppeurPascal/PicMobGenerator
+
+  ***************************************************************************
+  File last update : 2025-10-16T10:42:44.979+02:00
+  Signature : e2d9ef8c36c9fcfb66e1c9204f749253b163847a
+  ***************************************************************************
+*)
+
 unit uDM;
 
 interface
